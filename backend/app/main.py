6c2ad54d9aefd4bab7a284fbe4bffc72d925af3e
Gusthy02 <gusthyerris@gmail.com>
2025-09-16 19:32:09 -0300

    first commit

---
+++ backend/app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from app.routes import image_routes
+
+def create_app() -> FastAPI:
+    app = FastAPI(
+        title='Haircut API',
+        description='API for Haircut Simulation Project',
+        version='1.0.0'
+    )
+
+    # Registra rotas
+    app.include_router(image_routes.router, prefix='/image', tags=['Images'])
+    return app
+
+app = create_app()
